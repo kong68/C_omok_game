7f1880944e4e878b049046abc0b89d846620b72c
dhgus1231 <74444490+dhgus1231@users.noreply.github.com>
2020-11-29 21:37:16 +0900

    게임 마무리 수정

--- game_end.c
+++ game_end.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-int game_end(int Board[][20]) {
+int game_end(int(*Board)[19]) {
 	int i;
 	int j;
 	for (i = 2; i < 18; i++) {
@@ -7,26 +6,47 @@ int game_end(int Board[][20]) {
 			if (Board[j][i - 2] == 1 && Board[j][i - 1] == 1 && Board[j][i] == 1 && Board[j][i + 1] == 1 && Board[j][i + 2] == 1) {
 
 				printf("¼±°ø ½Â¸®");
-				getch();
+
 				return 1;
 			}
 			else if (Board[j][i - 2] == -1 && Board[j][i - 1] == -1 && Board[j][i] == -1 && Board[j][i + 1] == -1 && Board[j][i + 2] == -1) {
 
 				printf("ÈÄ°ø ½Â¸®");
-				getch();
+
 				return 1;
 			}
+		}
+	}
+	for (i = 2; i < 18; i++) {
+		for (j = 0; j < 20; j++) {
 
-			else if (Board[i - 2][j] == 1 && Board[i - 1][j] == 1 && Board[i][j] == 1 && Board[i + 1][j] == 1 && Board[i + 2][j] == 1) {
+			if (Board[i - 2][j] == 1 && Board[i - 1][j] == 1 && Board[i][j] == 1 && Board[i + 1][j] == 1 && Board[i + 2][j] == 1) {
 
 				printf("¼±°ø ½Â¸®");
-				getch();
+
 				return 1;
 			}
 			else if (Board[i - 2][j] == -1 && Board[i - 1][j] == -1 && Board[i][j] == -1 && Board[i + 1][j] == -1 && Board[i + 2][j] == -1) {
 
 				printf("ÈÄ°ø ½Â¸®");
-				getch();
+
+				return 1;
+			}
+		}
+	}
+
+
+	for (i = 2; i < 18; i++) {
+		for (j = 2; j < 18; j++) {
+			if (Board[j - 2][i - 2] == 1 && Board[j - 1][i - 1] == 1 && Board[j][i] == 1 && Board[j + 1][i + 1] == 1 && Board[j + 2][i + 2] == 1) {
+
+				printf("¼±°ø ½Â¸®");
+
+				return 1;
+			}
+			else if (Board[j - 2][i - 2] == -1 && Board[j - 1][i - 1] == -1 && Board[j][i] == -1 && Board[j + 1][i + 1] == -1 && Board[j + 2][i + 2] == -1) {
+				printf("ÈÄ°ø ½Â¸®");
+
 				return 1;
 			}
 		}
@@ -34,28 +54,17 @@ int game_end(int Board[][20]) {
 
 	for (i = 2; i < 18; i++) {
 		for (j = 2; j < 18; j++) {
-			if (Board[j - 2][i - 2] == 1 && Board[j - 1][i - 1] == 1 && Board[j][i] == 1 && Board[j + 1][i + 1] == 1 && Board[j + 2][i + 2] == 1) {
+
+			if (Board[j + 2][i - 2] == 1 && Board[j + 1][i - 1] == 1 && Board[j][i] == 1 && Board[j - 1][i + 1] == 1 && Board[j - 2][i + 2] == 1) {
 
 				printf("¼±°ø ½Â¸®");
-				getch();
-				return 1;
-			}
-			else if (Board[j - 2][i - 2] == -1 && Board[j - 1][i - 1] == -1 && Board[j][i] == -1 && Board[j + 1][i + 1] == -1 && Board[j + 2][i + 2] == -1) {
-				printf("ÈÄ°ø ½Â¸®");
-				getch();
-				return 1;
-			}
 
-			else if (Board[j + 2][i - 2] == 1 && Board[j + 1][i - 1] == 1 && Board[j][i] == 1 && Board[j - 1][i + 1] == 1 && Board[j - 2][i + 2] == 1) {
-
-				printf("¼±°ø ½Â¸®");
-				getch();
 				return 1;
 			}
 			else if (Board[j + 2][i - 2] == -1 && Board[j + 1][i - 1] == -1 && Board[j][i] == -1 && Board[j - 1][i + 1] == -1 && Board[j - 2][i + 2] == -1) {
 
 				printf("ÈÄ°ø ½Â¸®");
-				getch();
+
 				return 1;
 			}
 		}
