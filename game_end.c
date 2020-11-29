422f175187b639a9d8f3b553d7891916a6324510
dhgus1231 <74444490+dhgus1231@users.noreply.github.com>
2020-11-29 20:54:17 +0900

    게임 마무리 함수

---
+++ game_end.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+int game_end(int Board[][20]) {
+	int i;
+	int j;
+	for (i = 2; i < 18; i++) {
+		for (j = 0; j < 20; j++) {
+			if (Board[j][i - 2] == 1 && Board[j][i - 1] == 1 && Board[j][i] == 1 && Board[j][i + 1] == 1 && Board[j][i + 2] == 1) {
+
+				printf("¼±°ø ½Â¸®");
+				getch();
+				return 1;
+			}
+			else if (Board[j][i - 2] == -1 && Board[j][i - 1] == -1 && Board[j][i] == -1 && Board[j][i + 1] == -1 && Board[j][i + 2] == -1) {
+
+				printf("ÈÄ°ø ½Â¸®");
+				getch();
+				return 1;
+			}
+
+			else if (Board[i - 2][j] == 1 && Board[i - 1][j] == 1 && Board[i][j] == 1 && Board[i + 1][j] == 1 && Board[i + 2][j] == 1) {
+
+				printf("¼±°ø ½Â¸®");
+				getch();
+				return 1;
+			}
+			else if (Board[i - 2][j] == -1 && Board[i - 1][j] == -1 && Board[i][j] == -1 && Board[i + 1][j] == -1 && Board[i + 2][j] == -1) {
+
+				printf("ÈÄ°ø ½Â¸®");
+				getch();
+				return 1;
+			}
+		}
+	}
+
+	for (i = 2; i < 18; i++) {
+		for (j = 2; j < 18; j++) {
+			if (Board[j - 2][i - 2] == 1 && Board[j - 1][i - 1] == 1 && Board[j][i] == 1 && Board[j + 1][i + 1] == 1 && Board[j + 2][i + 2] == 1) {
+
+				printf("¼±°ø ½Â¸®");
+				getch();
+				return 1;
+			}
+			else if (Board[j - 2][i - 2] == -1 && Board[j - 1][i - 1] == -1 && Board[j][i] == -1 && Board[j + 1][i + 1] == -1 && Board[j + 2][i + 2] == -1) {
+				printf("ÈÄ°ø ½Â¸®");
+				getch();
+				return 1;
+			}
+
+			else if (Board[j + 2][i - 2] == 1 && Board[j + 1][i - 1] == 1 && Board[j][i] == 1 && Board[j - 1][i + 1] == 1 && Board[j - 2][i + 2] == 1) {
+
+				printf("¼±°ø ½Â¸®");
+				getch();
+				return 1;
+			}
+			else if (Board[j + 2][i - 2] == -1 && Board[j + 1][i - 1] == -1 && Board[j][i] == -1 && Board[j - 1][i + 1] == -1 && Board[j - 2][i + 2] == -1) {
+
+				printf("ÈÄ°ø ½Â¸®");
+				getch();
+				return 1;
+			}
+		}
+	}
+	return 0;
+}
