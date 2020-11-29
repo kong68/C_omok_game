008a7c20f66b72835d0e1a5c8fc723a9b62ef6fa
kong68 <74444856+kong68@users.noreply.github.com>
2020-11-29 21:39:09 +0900

    Update game_end.c
    세로검사, 왼쪽 아래에서 오른쪽 위 검사 오류 수정

--- game_end.c
+++ game_end.c
@@ -5,13 +5,13 @@ int game_end(int(*Board)[19]) {
 		for (j = 0; j < 20; j++) {
 			if (Board[j][i - 2] == 1 && Board[j][i - 1] == 1 && Board[j][i] == 1 && Board[j][i + 1] == 1 && Board[j][i + 2] == 1) {
 
-				printf("¼±°ø ½Â¸®");
+				printf("ì„ ê³µ ìŠ¹ë¦¬");
 
 				return 1;
 			}
 			else if (Board[j][i - 2] == -1 && Board[j][i - 1] == -1 && Board[j][i] == -1 && Board[j][i + 1] == -1 && Board[j][i + 2] == -1) {
 
-				printf("ÈÄ°ø ½Â¸®");
+				printf("í›„ê³µ ìŠ¹ë¦¬");
 
 				return 1;
 			}
@@ -22,13 +22,13 @@ int game_end(int(*Board)[19]) {
 
 			if (Board[i - 2][j] == 1 && Board[i - 1][j] == 1 && Board[i][j] == 1 && Board[i + 1][j] == 1 && Board[i + 2][j] == 1) {
 
-				printf("¼±°ø ½Â¸®");
+				printf("ì„ ê³µ ìŠ¹ë¦¬");
 
 				return 1;
 			}
 			else if (Board[i - 2][j] == -1 && Board[i - 1][j] == -1 && Board[i][j] == -1 && Board[i + 1][j] == -1 && Board[i + 2][j] == -1) {
 
-				printf("ÈÄ°ø ½Â¸®");
+				printf("í›„ê³µ ìŠ¹ë¦¬");
 
 				return 1;
 			}
@@ -40,12 +40,12 @@ int game_end(int(*Board)[19]) {
 		for (j = 2; j < 18; j++) {
 			if (Board[j - 2][i - 2] == 1 && Board[j - 1][i - 1] == 1 && Board[j][i] == 1 && Board[j + 1][i + 1] == 1 && Board[j + 2][i + 2] == 1) {
 
-				printf("¼±°ø ½Â¸®");
+				printf("ì„ ê³µ ìŠ¹ë¦¬");
 
 				return 1;
 			}
 			else if (Board[j - 2][i - 2] == -1 && Board[j - 1][i - 1] == -1 && Board[j][i] == -1 && Board[j + 1][i + 1] == -1 && Board[j + 2][i + 2] == -1) {
-				printf("ÈÄ°ø ½Â¸®");
+				printf("í›„ê³µ ìŠ¹ë¦¬");
 
 				return 1;
 			}
@@ -57,13 +57,13 @@ int game_end(int(*Board)[19]) {
 
 			if (Board[j + 2][i - 2] == 1 && Board[j + 1][i - 1] == 1 && Board[j][i] == 1 && Board[j - 1][i + 1] == 1 && Board[j - 2][i + 2] == 1) {
 
-				printf("¼±°ø ½Â¸®");
+				printf("ì„ ê³µ ìŠ¹ë¦¬");
 
 				return 1;
 			}
 			else if (Board[j + 2][i - 2] == -1 && Board[j + 1][i - 1] == -1 && Board[j][i] == -1 && Board[j - 1][i + 1] == -1 && Board[j - 2][i + 2] == -1) {
 
-				printf("ÈÄ°ø ½Â¸®");
+				printf("í›„ê³µ ìŠ¹ë¦¬");
 
 				return 1;
 			}
